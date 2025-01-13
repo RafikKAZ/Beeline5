0249328eda94755aa1c5804d0d50ffe6d166acce
RafikKAZ <rafikazer89@gmail.com>
2025-01-13 15:17:35 +0500

    Update script.js

--- script.js
+++ script.js
@@ -44,12 +44,41 @@ document.addEventListener("DOMContentLoaded", function () {
             zoom: 10
         });
 
+        map.events.add("click", function (e) {
+            const coords = e.get("coords");
+            if (placemark) {
+                placemark.geometry.setCoordinates(coords);
+            } else {
+                placemark = createPlacemark(coords);
+                map.geoObjects.add(placemark);
+            }
+            getAddress(coords); // Получение адреса
+        });
+
         // Обработчик изменения города
         document.getElementById("city").addEventListener("change", function () {
             const selectedCity = this.value;
             if (cityCenters[selectedCity]) {
                 map.setCenter(cityCenters[selectedCity], 10); // Устанавливаем центр карты
             }
+        });
+    }
+
+    function createPlacemark(coords) {
+        return new ymaps.Placemark(coords, {}, {
+            preset: "islands#blueDotIcon",
+            draggable: false
+        });
+    }
+
+    function getAddress(coords) {
+        ymaps.geocode(coords).then(function (res) {
+            const firstGeoObject = res.geoObjects.get(0);
+            const address = firstGeoObject.getAddressLine();
+            document.getElementById("address").value = address; // Заполнение поля "Адрес дома"
+            document.getElementById("coordinates").value = coords.join(", "); // Заполнение поля "Координаты"
+        }).catch(function (err) {
+            console.error("Ошибка при определении адреса:", err);
         });
     }
 });
